004bf5a221783161e684ea08dc00b35fb2676167
Allen Francis <lallenfrancisl@gmail.com>
2021-09-08 00:30:29 +0530

    build: remove tailwindcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,5 @@
 const postcssPresetEnv = require('postcss-preset-env');
-const tailwindcss = require('tailwindcss');
 
 module.exports = {
-  plugins: [postcssPresetEnv(), tailwindcss(), ]
+  plugins: [postcssPresetEnv()]
 };

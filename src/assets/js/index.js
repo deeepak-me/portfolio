4b6c62f85c7bc97bb85747a390c64cf088c26e1d
Deepak Krishnan E <deepak.krishnan.e@gmail.com>
2022-09-19 01:11:10 +0530

    feat: card sliding implemented

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,20 @@
-console.log('Hello world');
+const slides = document.querySelector('.client-slide');
+const button = document.querySelector('.nxt-btn');
+let numberOfSlides = document.querySelectorAll('.client-card').length;
+
+console.log(numberOfSlides);
+
+let slideIndex = 1;
+let translateX = 0;
+
+
+button.addEventListener('click', e => {
+    if (e.target.id === 'next') {
+        if (slideIndex !== numberOfSlides) {
+            slideIndex++;
+            translateX -= 362;
+        }
+    }
+    slides.style.transform = `translateX(${translateX}px)`;
+})
+
